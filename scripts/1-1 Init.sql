5891d9fa402b13f39b53f6bad27a8d265d78e06e
n.bacherikov <n.bacherikov@simcord.com>
2025-10-14 23:42:28 +0300

    add ddl scripts

---
+++ scripts/1-1 Init.sql
@@ -0,0 +1,26 @@
+create table users
+(
+    id   uuid,
+    name varchar not null check ( CHAR_LENGTH(name) > 2 ),
+    constraint users_pk primary key (id)
+);
+
+create table user_activity
+(
+    id          uuid,
+    user_id     uuid    not null,
+    action_date timestamp(3) not null,
+    action      varchar not null,
+    metadata    jsonb,
+    constraint user_activity_pk primary key (id),
+    constraint user_activity_users_fk foreign key (user_id) references users (id) on delete cascade
+);
+
+create table user_activity_history
+(
+    id            uuid,
+    user_id       uuid         not null,
+    from_date     timestamp(3) not null,
+    to_date       timestamp(3) not null check ( to_date > from_date ),
+    actions_count bigint default 0 check ( actions_count >= 0 )
+);
